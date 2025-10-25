0a1f83f61c1d19c5f2d75d7d513fff68c81869fe
Vlad <3vlad201711111111@gmail.com>
2025-10-25 13:32:46 +0300

    Add Description file - explain structure of training schema and project
    Add new button "current schema"
    
    Add ViewModel to MainPaje

---
+++ Tranee/viewModels/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace Tranee.viewModels
+{
+    internal class MainPageViewModel : INotifyPropertyChanged 
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
