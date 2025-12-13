a391589014d4ddb1bffcc457da8ee292c806bdf9
Vlad <3vlad201711111111@gmail.com>
2025-12-13 02:06:27 +0200

    create a plan with TODOS

--- Tranee/MauiProgram.cs
+++ Tranee/MauiProgram.cs
@@ -9,6 +9,21 @@ namespace Tranee
 {
     public static class MauiProgram
     {
+        //TODOS: Make a page for creating Template
+        //TODOS: Make a detailed Templates
+        //TODOS: Make a detailed history (like menu in template) when see group  of muscle and data and button to see all information about training
+       
+        //TODOS: create a enums for muscle group and quality and change everywhere it used
+        
+        //TODOS: make a analize page with functional
+
+        //TODOS: Make pretty ui for app with animations, image 
+
+        //TODOS: Make API
+        //TODOS: Make MySql Database
+        //TODOS: Make Authorization
+        //TODOS: Deploy ALL of it On AZURE CLOUD
+
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
